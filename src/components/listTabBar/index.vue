beb1ce3cbf36be09bd42828289d2a73c8424fc57
mayyingzi <3205009763@qq.com>
2018-03-14 17:45:20 +0800

    add tabbar component doing

--- src/components/listTabBar/index.vue
+++ src/components/listTabBar/index.vue
@@ -2,13 +2,69 @@
     <div class="barMain">
         <div class="bar">
             <div class="item" 
-                v-for="(value, index) in ['1', '2', '3']"
-                :class="{'actived':index===0}"
-                :key="value">
-                <p class="text">所有店铺</p>
-            </div>
+                v-for="(item, index) in curTabVal"
+                :class="{'actived':index===actTab}"                
+                :key="index">
+                <div 
+                    @click.stop="toggleDetail(index)"
+                    class="tabown">
+                    <p class="text">{{item.title}}</p>
+                </div>
+                <!-- tab对应列表 -->
+                <div class="tabDetail">
+                    <div 
+                        v-if="index > 0"
+                        class="detailMain">
+                        <div class="mainCell">
+                            <div class="cellItem"
+                                v-if="item.key==='shelvesGoodsStatus'"
+                                :class="{'selected': item.cur===-1}"
+                                @click="selectTabCell({
+                                    cur:-1,
+                                    title:'所有状态',
+                                    tabIndex: index})"
+                            >
+                                <p>所有状态</p>
+                            </div>
+                            <div class="cellItem"
+                                v-if="item.key==='pigTypeMap'"
+                                :class="{'selected': item.cur===-1}"
+                                @click="selectTabCell({
+                                    cur:-1,
+                                    title:'所有品类',
+                                    tabIndex: index})"
+                            >
+                                <p>所有品类</p>
+                            </div>
+                            <div class="cellItem"
+                                v-for="(cellI, cellKey) in listData[item.key]"
+                                :key="cellKey"
+                                :class="{'selected': item.cur===cellKey}"
+                                @click="selectTabCell({
+                                    cur:cellKey,
+                                    title:cellI,
+                                    tabIndex: index})"
+                            >
+                                <p>{{cellI}}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div 
+                        v-else
+                        class="detailMain">
+                        <div class="mainPar clos">
+                            <div class="lineCell slected">位置</div>
+                            <div class="lineCell">所有店铺</div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
         </div>
-        <div class="bgCover"></div>
+        <div 
+            @click="toggleDetail(-1)"
+            :class="{'show': actTab !=null}"
+            class="bgCover"></div>
     </div>
 </template>
 
--- src/components/listTabBar/listTabBar.js
+++ src/components/listTabBar/listTabBar.js
@@ -4,21 +4,104 @@ export default {
     name: 'listBarTab',
     props: ['listBarData'],
     data() {
-        return {};
+        return {
+            curTabVal: [
+                {
+                    key: 'firTab',
+                    cur: -1,
+                    selected: {
+                        shop:{
+                            index: -1,
+                        },
+                        addr:{
+                            provinceInd: -1,
+                            cityInd: -1
+                        }
+                    },
+                    postVal: {
+                        shopId: '',
+                        provinceName: '',
+                        cityName: '',
+                        kilometers: ''
+                    },
+                    title: '所有店铺'
+                },
+                {
+                    key: 'shelvesGoodsStatus',
+                    postVal: {
+                        goodsStatus: ''
+                    },
+                    cur: -1,
+                    title: '所有状态'
+                },
+                {
+                    key: 'pigTypeMap',
+                    postVal: {
+                        goodsType: ''
+                    },
+                    cur: -1,
+                    title: '所有品类'
+                }
+            ],
+            actTab: null
+        };
     },
     created() {},
     computed: {
         listData() {
             const result = {};
             if (this.listBarData) {
+                const keyList = [
+                    'pigTypeMap',
+                    'shelvesGoodsStatus',
+                    'shopMap',
+                    'address'
+                ];
+                const omitKey = [keyList[2], keyList[3]];
+                const _result = _.extend({}, this.listBarData);
                 result.firTab = {
-                    shopMap: this.listBarData.shopMap,
-                    address: this.listBarData.address
+                    shopMap: _result.shopMap,
+                    address: _result.address
                 };
-                _.extend(result, _.omit(this.listBarData, ['shopMap', 'address']));
+                _.extend(result, _.omit(_result, omitKey));
             }
-
             return result;
+        }
+    },
+    watch: {
+        curTabVal: {
+            deep: true,
+            handler(newVal) {
+                console.log('重新更新接口');
+                console.log(newVal);
+            }
+        }
+    },
+    methods: {
+        toggleDetail(actTabIndx) {
+            if (this.actTab === actTabIndx || actTabIndx === -1) {
+                this.actTab = null;
+                return;
+            }
+            this.actTab = actTabIndx;
+        },
+        selectTabCell(config) {
+            const { title, cur, tabIndex } = config;
+            if (this.curTabVal[tabIndex].cur === cur) {
+                this.toggleDetail(-1);
+                return;
+            }
+            const postValKey = ['firTab', 'goodsStatus', 'goodsType'];
+            const result = {
+                cur,
+                title,
+                postVal: {}
+            };
+            if (tabIndex > 0) {
+                result.postVal[postValKey[tabIndex]] = cur !== -1 ? cur : '';
+            }
+            _.extend(this.curTabVal[tabIndex], result);
+            this.toggleDetail(-1);
         }
     }
 };
